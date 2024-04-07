12143a2f1ff5ce2e8b0ab8cda9332e6d9b218293
venkat.viswanathan <dfe2zj@virginia.edu>
2024-04-07 23:59:00 +0530

    initial version

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='booklover',
+      version='0.1',
+      description='Best book lover app',
+      url='https://github.com/venkatviswa/hw09repo',
+      author='Venkat',
+      author_email='dfe2zj@virginia.edu',
+      license='MIT',
+      packages=['hwpkg'])
